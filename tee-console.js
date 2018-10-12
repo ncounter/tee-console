11ada0182b02a72e036be2f73aab0b18d4c27d5d
dleidi <dleidi@suse.com>
2018-10-12 17:34:26 +0200

    Fix default prefix message

--- tee-console.js
+++ tee-console.js
@@ -3,19 +3,19 @@ const myConsole = (function(defaultConsole){
   return {
     log: function(message){
       defaultConsole.log(message);
-      alert('[Tee-console] Log: ' + message);
+      alert('[Tee-console - Log]: ' + message);
     },
     info: function (message) {
       defaultConsole.info(message);
-      alert('[Tee-console] Info: ' + message);
+      alert('[Tee-console - Info]: ' + message);
     },
     warn: function (message) {
       defaultConsole.warn(message);
-      alert('[Tee-console] Warning: ' + message);
+      alert('[Tee-console - Warning]: ' + message);
     },
     error: function (message) {
       defaultConsole.error(message);
-      alert('[Tee-console] Error: ' + message);
+      alert('[Tee-console - Error]: ' + message);
     }
   };
 }(window.console));
