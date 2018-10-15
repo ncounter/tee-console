61191bbba0bac0e87813ad3b296b9ddfb9df98fa
dleidi <dleidi@suse.com>
2018-10-15 09:37:43 +0200

    Clone the console object first to keep other default functionalities

--- tee-console.js
+++ tee-console.js
@@ -1,27 +1,30 @@
 // wrap and re-implement default console methods
 const myConsole = (function(defaultConsole){
+  const clonedConsole = Object.assign({}, defaultConsole);
+
   let myConsoleAction = () => {};
-  return {
-    setMyConsoleAction: function(f) {
-      myConsoleAction = f;
-    },
-    log: function(message){
-      defaultConsole.log(message);
-      myConsoleAction('[Tee-console - Log]: ' + message);
-    },
-    info: function (message) {
-      defaultConsole.info(message);
-      myConsoleAction('[Tee-console - Info]: ' + message);
-    },
-    warn: function (message) {
-      defaultConsole.warn(message);
-      myConsoleAction('[Tee-console - Warning]: ' + message);
-    },
-    error: function (message) {
-      defaultConsole.error(message);
-      myConsoleAction('[Tee-console - Error]: ' + message);
-    }
-  };
+
+  clonedConsole.setMyConsoleAction = function(f) {
+    myConsoleAction = f;
+  }
+  clonedConsole.log = function(message){
+    defaultConsole.log(message);
+    myConsoleAction('[Tee-console - Log]: ' + message);
+  }
+  clonedConsole.info = function (message) {
+    defaultConsole.info(message);
+    myConsoleAction('[Tee-console - Info]: ' + message);
+  }
+  clonedConsole.warn = function (message) {
+    defaultConsole.warn(message);
+    myConsoleAction('[Tee-console - Warning]: ' + message);
+  }
+  clonedConsole.error = function (message) {
+    defaultConsole.error(message);
+    myConsoleAction('[Tee-console - Error]: ' + message);
+  }
+
+  return clonedConsole;
 }(window.console));
 
 // Override the existing default console
